45c1199fe5f08d8df435742c3cd4e2cf09090fdf
Tommy Yuan <tommy.yuan@york.ac.uk>
2022-01-26 10:18:50 +0000

    "First message today in Jan"

--- mavenTest/src/test/java/eng1/mavenTest/AppTest.java
+++ mavenTest/src/test/java/eng1/mavenTest/AppTest.java
@@ -17,5 +17,11 @@ public class AppTest
         assertTrue( true );
     } 
     
+    @Test
+    public void testShouldAnswerWithFalse26jan2022()
+    {
+        assertTrue( false );
+    } 
+    
     
 }
